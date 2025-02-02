826708a57a78dcbe8b733e5fc6c40fcc86ba1328
Tuan Le <leanhtuan030523@gmail.com>
2025-02-02 13:40:50 +0700

    Create AuthHandler with LoginPage

---
+++ handlers/auth.go
@@ -0,0 +1,12 @@
+package handlers
+
+import (
+	"github.com/TuanLe53/Go-HTMX-URL-Shortener/templates/pages"
+	"github.com/labstack/echo/v4"
+)
+
+type AuthHandler struct{}
+
+func (h AuthHandler) LoginPage(c echo.Context) error {
+	return Render(c, pages.Login())
+}
