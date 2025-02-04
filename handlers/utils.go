2c64f816c2244eddd826694a809b387aa732b2df
Tuan Le <leanhtuan030523@gmail.com>
2025-02-04 12:18:45 +0700

    Create IsValidEmail func

--- handlers/utils.go
+++ handlers/utils.go
@@ -1,10 +1,18 @@
 package handlers
 
 import (
+	"regexp"
+
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 )
 
 func Render(c echo.Context, component templ.Component) error {
 	return component.Render(c.Request().Context(), c.Response())
+}
+
+func IsValidEmail(email string) bool {
+	const emailRegex = `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	re := regexp.MustCompile(emailRegex)
+	return re.MatchString(email)
 }
