65ca0a5bf3dcc87bd514def6963702273f2d268c
Tuan Le <leanhtuan030523@gmail.com>
2025-02-04 12:17:26 +0700

    Create FindUserWithEmail and CreateUser func

--- db/models/user.go
+++ db/models/user.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"errors"
+
+	"github.com/TuanLe53/Go-HTMX-URL-Shortener/db"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
@@ -12,4 +15,29 @@ type User struct {
 	FirstName string    `gorm:"type:varchar(100);not null"`
 	LastName  string    `gorm:"type:varchar(100);not null"`
 	Password  string    `gorm:"type:varchar(255);not null"`
+}
+
+func FindUserWithEmail(email string) (*User, error) {
+	db := db.DB()
+
+	var user User
+	result := db.Where("email = ?", email).First(&user)
+	if result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, errors.New("error looking for user")
+	} else {
+		return &user, nil
+	}
+}
+
+func CreateUser(email, firstName, lastName, password string) *User {
+	db := db.DB()
+
+	user := User{Email: email, FirstName: firstName, LastName: lastName, Password: password}
+
+	db.Create(&user)
+
+	return &user
 }
