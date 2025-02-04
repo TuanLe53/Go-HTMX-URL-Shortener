67b37e19f0352b15efa006c9500db828144c8176
Tuan Le <leanhtuan030523@gmail.com>
2025-02-04 12:18:12 +0700

    Create RegisterUser handler

--- handlers/auth.go
+++ handlers/auth.go
@@ -1,6 +1,11 @@
 package handlers
 
 import (
+	"net/http"
+
+	"github.com/TuanLe53/Go-HTMX-URL-Shortener/db/models"
+	"github.com/TuanLe53/Go-HTMX-URL-Shortener/pkg/auth"
+	"github.com/TuanLe53/Go-HTMX-URL-Shortener/templates/components"
 	"github.com/TuanLe53/Go-HTMX-URL-Shortener/templates/pages"
 	"github.com/labstack/echo/v4"
 )
@@ -13,4 +18,33 @@ func (h AuthHandler) LoginPage(c echo.Context) error {
 
 func (h AuthHandler) RegisterPage(c echo.Context) error {
 	return Render(c, pages.Register())
+}
+
+func (h AuthHandler) RegisterUser(c echo.Context) error {
+	email := c.FormValue("email")
+	firstName := c.FormValue("first_name")
+	lastName := c.FormValue("last_name")
+	password := c.FormValue("password")
+
+	if !IsValidEmail(email) {
+		return Render(c, components.ErrorMessage("Invalid email"))
+	}
+
+	isUserExists, err := models.FindUserWithEmail(email)
+	if err != nil {
+		return Render(c, components.ErrorMessage(err.Error()))
+	}
+	if isUserExists != nil {
+		return Render(c, components.ErrorMessage("Email already taken."))
+	}
+
+	hashedPW, err := auth.HashPw(password)
+	if err != nil {
+		return Render(c, components.ErrorMessage(err.Error()))
+	}
+
+	models.CreateUser(email, firstName, lastName, hashedPW)
+
+	c.Response().Header().Set("hx-redirect", "/login")
+	return c.NoContent(http.StatusSeeOther)
 }
