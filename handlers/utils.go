b857ea2c3fb89248ce79b73280fa49267bdd9732
Tuan Le <leanhtuan030523@gmail.com>
2025-02-06 15:05:15 +0700

    Create CreateCookie function

--- handlers/utils.go
+++ handlers/utils.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"fmt"
+	"net/http"
 	"regexp"
+	"time"
 
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
@@ -15,4 +18,22 @@ func IsValidEmail(email string) bool {
 	const emailRegex = `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
 	re := regexp.MustCompile(emailRegex)
 	return re.MatchString(email)
+}
+
+func CreateCookie(name, value string, minutes int) (*http.Cookie, error) {
+	if name == "" || value == "" {
+		return nil, fmt.Errorf("cookie name and value cannot be empty")
+	}
+	if minutes <= 0 {
+		return nil, fmt.Errorf("minutes must be a positive number")
+	}
+
+	return &http.Cookie{
+		Name:    name,
+		Value:   value,
+		Expires: time.Now().Add(time.Duration(minutes) * time.Minute),
+		// Path:     path,
+		// Domain:   domain,
+		HttpOnly: true,
+	}, nil
 }
